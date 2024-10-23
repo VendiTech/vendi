cb7cae3f99abc1b1a58af1774bb7e4475f56dca5
roman.gorlach <roman.gorlach@ai-sft.dev>
2024-10-23 19:45:47 +0200

    feature: Add Nayax Schemas

--- backend/core/helpers.py
+++ backend/core/helpers.py
@@ -22,3 +22,17 @@ def boolify(value: Any) -> bool:
     ]
 
     return str(value).lower() not in false_strings
+
+
+def to_title_case(field_name: str) -> str:
+    """
+    Convert field name to a title case.
+
+    Example:
+    >>> "use_phone_contact" -> "Use Phone Contact"
+
+    :param field_name: Field name to be converted.
+
+    :return: Converted field name.
+    """
+    return " ".join([item.capitalize() for item in field_name.split("_")])
