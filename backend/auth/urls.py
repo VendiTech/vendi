36cbbb55903c2a6b95f4cae5744e4aa7ff01fdd2
roman.gorlach <roman.gorlach@ai-sft.dev>
2024-10-24 13:18:22 +0200

    feature: Update urls.py

--- backend/auth/urls.py
+++ backend/auth/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from auth.views import DecoratedTokenObtainPairView, DecoratedTokenRefreshView, RegisterView
 
 urlpatterns = [
-    path("api/token/", DecoratedTokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", DecoratedTokenRefreshView.as_view(), name="token_refresh"),
-    path("api/register/", RegisterView.as_view(), name="register"),
+    path("token/", DecoratedTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", DecoratedTokenRefreshView.as_view(), name="token_refresh"),
+    path("register/", RegisterView.as_view(), name="register"),
 ]
