cc0506926154b166264a06ffdcf907701558b643
roman.gorlach <roman.gorlach@ai-sft.dev>
2024-10-24 14:14:35 +0200

    feature: Refactor the structure

--- backend/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-api_urls = [path("auth/", include("auth.urls")), path("", include("core.swagger.urls"))]
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include(api_urls)),
-]
---
+++ mspy_vendi/consumers/nayax_consumer.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+
+from mspy_vendi.core.logger import LOG
+from mspy_vendi.sqs.consumer import SQSConsumer
+
+if __name__ == "__main__":
+    SQSConsumer(
+        sqs_queue_name=settings.SQS_QUEUE_NAME,
+        sqs_max_number_of_messages=settings.SQS_MAX_NUMBER_OF_MESSAGES,
+        sqs_visibility_timeout=settings.SQS_VISIBILITY_TIMEOUT,
+        sqs_long_poll_time=settings.SQS_LONG_POLL_TIME,
+        sqs_auto_ack=settings.SQS_AUTO_ACK,
+        logger=LOG,
+    ).consume()
