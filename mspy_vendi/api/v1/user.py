aad2f8e5385be39f309e2731d15478bc6a921a4c
roman.gorlach <roman.gorlach@innowise.com>
2024-10-29 15:27:05 +0100

    feature: Add FastAPI support

--- mspy_vendi/api/v1/user.py
+++ mspy_vendi/api/v1/user.py
@@ -1,9 +1,12 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends
 from fastapi.responses import ORJSONResponse
 
 from mspy_vendi.core.api import CRUDApi
 from mspy_vendi.core.pagination import Page
 from mspy_vendi.core.enums import CRUDEnum
+from mspy_vendi.domain.user.models import User
 
 from mspy_vendi.domain.user.schemas import UserDetail, UserUpdate
 from mspy_vendi.domain.user.services import UserService
@@ -13,6 +16,17 @@ from mspy_vendi.domain.auth import get_current_user
 
 router = APIRouter(prefix="/user", default_response_class=ORJSONResponse, tags=[ApiTagEnum.USER])
 
+@router.get("/me", response_model=UserDetail)
+async def get__show_me(
+    user: Annotated[User, Depends(get_current_user())],
+    service: Annotated[UserService, Depends()],
+) -> UserDetail:
+    """
+    Retrieve the User by ID.
+
+    - **user_id**: User ID
+    """
+    return await service.get(obj_id=user.id)
 
 class UserAPI(CRUDApi):
     service = UserService
@@ -20,7 +34,7 @@ class UserAPI(CRUDApi):
     update_schema = UserUpdate
     get_db_session = Depends(get_db_session)
     current_user_mapping = {
-        CRUDEnum.GET: Depends(get_current_user()),
+        CRUDEnum.GET: Depends(get_current_user(is_superuser=True)),
         CRUDEnum.LIST: Depends(get_current_user(is_superuser=True)),
         CRUDEnum.UPDATE: Depends(get_current_user()),
     }
