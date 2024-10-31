6b5375e14843cd25e83f5fcc26e8e594ff9c3a88
roman.gorlach <roman.gorlach@innowise.com>
2024-10-31 13:49:24 +0100

    feature: Add cookie auth

--- mspy_vendi/api/auth_backend.py
+++ mspy_vendi/api/auth_backend.py
@@ -1,12 +1,17 @@
-from fastapi_users.authentication import AuthenticationBackend, BearerTransport, JWTStrategy
+from fastapi_users.authentication import AuthenticationBackend, CookieTransport, JWTStrategy
 
 from mspy_vendi.config import config
 
-bearer_transport = BearerTransport(tokenUrl=config.login_url)
+cookie_transport = CookieTransport(
+    cookie_name=config.auth_cookie_name,
+    cookie_max_age=config.token_lifetime,
+    cookie_samesite="none",
+    cookie_secure=config.auth_cookie_secure,
+)
 
 
 def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(secret=config.secret_key, lifetime_seconds=config.token_lifetime)
 
 
-backend = AuthenticationBackend(name="jwt", transport=bearer_transport, get_strategy=get_jwt_strategy)
+backend = AuthenticationBackend(name="jwt", transport=cookie_transport, get_strategy=get_jwt_strategy)
