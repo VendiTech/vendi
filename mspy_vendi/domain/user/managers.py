efb0aca5cbd3be84fb771aaa7751515c152464e8
roman.gorlach <roman.gorlach@innowise.com>
2024-11-18 17:21:15 +0100

    feature: Add permissions

--- mspy_vendi/domain/user/managers.py
+++ mspy_vendi/domain/user/managers.py
@@ -7,6 +7,16 @@ class UserManager(CRUDManager):
     sql_model = User
 
     async def update_permissions(self, modified_user: User, permissions: list[PermissionEnum]) -> User:
+        """
+        Update the permissions of the user.
+        Check if the permissions are already present in the user's permissions.
+        And merge the permissions with the user's permissions.
+
+        :param modified_user: The user to update.
+        :param permissions: The permissions to update.
+
+        :return: The updated user.
+        """
         updated_permissions: set[PermissionEnum] = set(modified_user.permissions) | set(permissions)
 
         modified_user.permissions = list(updated_permissions)
@@ -16,6 +26,16 @@ class UserManager(CRUDManager):
         return modified_user
 
     async def delete_permissions(self, modified_user: User, permissions: list[PermissionEnum]) -> User:
+        """
+        Delete the permissions of the user.
+        Check if the permissions are already present in the user's permissions.
+        And subtract the permissions with the user's permissions.
+
+        :param modified_user: The user to update.
+        :param permissions: The permissions to update.
+
+        :return: The updated user.
+        """
         updated_permissions: set[PermissionEnum] = set(modified_user.permissions) - set(permissions)
 
         modified_user.permissions = list(updated_permissions)
