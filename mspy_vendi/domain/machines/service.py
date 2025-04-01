18406f710e668dfe35017d4c4cc7b6b05fef70b0
roman.gorlach <roman.gorlach@innowise.com>
2025-04-01 19:56:37 +0200

    Feature: Implement dedicated endpoint to fetch Geographies and located machines

--- mspy_vendi/domain/machines/service.py
+++ mspy_vendi/domain/machines/service.py
@@ -2,7 +2,7 @@ from mspy_vendi.core.pagination import Page
 from mspy_vendi.core.service import CRUDService
 from mspy_vendi.domain.machines.filters import MachineFilter
 from mspy_vendi.domain.machines.manager import MachineManager
-from mspy_vendi.domain.machines.schemas import MachinesCountGeographySchema
+from mspy_vendi.domain.machines.schemas import MachinesCountGeographySchema, MachinesPerGeographySchema
 from mspy_vendi.domain.user.models import User
 
 
@@ -14,3 +14,8 @@ class MachineService(CRUDService):
         self, query_filter: MachineFilter, user: User
     ) -> Page[MachinesCountGeographySchema]:
         return await self.manager.get_machines_count_per_geography(query_filter, user)
+
+    async def get_machines_by_geography(
+        self, query_filter: MachineFilter, user: User
+    ) -> Page[MachinesPerGeographySchema]:
+        return await self.manager.get_machines_by_geography(query_filter, user)
