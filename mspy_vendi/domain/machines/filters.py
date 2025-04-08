cd11741c6dc2c69720b60359ddde5b443b9941a6
roman.gorlach <roman.gorlach@innowise.com>
2025-04-08 12:20:16 +0200

    Feature: Add geography_name into MachineFilter

--- mspy_vendi/domain/machines/filters.py
+++ mspy_vendi/domain/machines/filters.py
@@ -1,15 +1,17 @@
+from fastapi_filter import FilterDepends, with_prefix
 from pydantic import PositiveInt
 
 from mspy_vendi.core.filter import BaseFilter
+from mspy_vendi.domain.geographies.filters import GeographyFilter
 from mspy_vendi.domain.machines.models import Machine
 
 
 class MachineFilter(BaseFilter):
     id__in: list[PositiveInt] | None = None
-    geography_id__in: list[PositiveInt] | None = None
 
     name: str | None = None
     geography_id: int | None = None
+    geography: GeographyFilter | None = FilterDepends(with_prefix("geography", GeographyFilter))
 
     order_by: list[str] | None = ["-id"]
 
